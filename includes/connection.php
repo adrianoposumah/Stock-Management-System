8d5c98c82c87efd8732fc31d3a46ff5727b7380b
adrianoposumah <113793119+adrianoposumah@users.noreply.github.com>
2024-10-13 17:20:14 +0800

    my first commit

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
- $db = mysqli_connect('localhost', 'root', '') or
+ $db = mysqli_connect('localhost', 'root', '020804apip') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'scms' ) or die(mysqli_error($db));
         
